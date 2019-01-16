a911c20e304fbd856367b047ed26b6b197376fab
airo1396 <46729719+airo1396@users.noreply.github.com>
2019-01-16 10:26:25 -0800

    Add files via upload

---
+++ sim.py
@@ -0,0 +1,8 @@
+Import player
+from random import uniform, random, randint
+from datetime import datetime, timedelta
+
+TURNOVERS = ['pass stolen by', 'drive stripped by', '3 pointer blocked by', \
+    'mid-range jumpshot blocked by', 'inside shot blocked by''drive blocked by']
+MISSED_SHOTS = ['Layup missed by', 'Dunk missed by', 'Inside shot missed by', \
+                'Mid-range jumper missed by', '3 pointer missed by']
